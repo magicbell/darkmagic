7073a43d2b489a530fc64efacd3db95fd2529588
Stephan Meijer <stephan.meijer@gmail.com>
2023-01-23 14:18:01 +0100

    chore: remove compiled file

--- example/index.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import 'react-app-polyfill/ie11';
-import * as ReactDOM from 'react-dom';
-function App() {
-    return _jsx("div", { id: "target", children: "example app" });
-}
-ReactDOM.render(_jsx(App, {}), document.getElementById('root'));
