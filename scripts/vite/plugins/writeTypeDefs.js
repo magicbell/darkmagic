ca79a5d8d951679b0b3dc9a41aba1e1edebb5d8f
Stephan Meijer <stephan.meijer@gmail.com>
2023-01-23 14:00:51 +0100

    chore: add build message

--- scripts/vite/plugins/writeTypeDefs.js
+++ scripts/vite/plugins/writeTypeDefs.js
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { execSync } from 'child_process';
 
 import { pkg } from '../settings.js';
@@ -11,6 +12,8 @@ export async function writeTypeDefs() {
         this.warn(`package ${pkg.name} does not export any types. Set package.json#typings if this is a mistake.`);
         return;
       }
+
+      process.stdout.write(`\n${chalk.cyan(`tsc`)} ${chalk.gray(`generating types`)}\n`);
       execSync(`tsc --emitDeclarationOnly --declaration --noEmit false --project tsconfig.build.json --outDir dist`);
     },
   };
