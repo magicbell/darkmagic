b139a1abd408c3dfd530036cd1de99a8c805b883
Ullrich Schäfer <hi+github@ullrich.is>
2025-01-28 17:40:27 +0100

    chore: Adding Storybook example on how to combine Tooltip with ToggleGroup (#172)
    Co-authored-by: Stephan Meijer <stephan.meijer@gmail.com>

--- packages/react/src/components/tooltip.stories.tsx
+++ packages/react/src/components/tooltip.stories.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import { Flex } from './flex';
 import { IconButton } from './icon-button';
+import { ToggleGroup } from './toggle-group';
 import { Tooltip } from './tooltip';
 
 const meta: Meta = {
@@ -36,3 +37,16 @@ export const Right = { args: { side: 'right' } };
 export const Top = { args: { side: 'top' } };
 
 export const Bottom = { args: { side: 'bottom' } };
+
+export const OnToggle = (args: any) => (
+  <ToggleGroup size="md" type="single">
+    <Tooltip {...args}>
+      {/* Wrapping the children in Flex prevents clash on radix-uis internal `data-state` prop */}
+      <Flex>
+        <ToggleGroup.Item width="hug" value="all">
+          Toggle
+        </ToggleGroup.Item>
+      </Flex>
+    </Tooltip>
+  </ToggleGroup>
+);
